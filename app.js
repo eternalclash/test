0079d3d7da9196006ad40f018954ef38583d2846
eternalclash <suchasucha@naver.com>
2023-06-02 20:22:39 +0900

    수정

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ const { App } = require('@slack/bolt');
 const http = require('http');
 const app_server =require('express')
 const { WebClient } = require('@slack/web-api');
+const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
 const socket_server = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
 });
-
+console.log(process.env.SLACK_APP_TOKEN)
+console.log('token:', socket_server.token);
+console.log('signingSecret:', socket_server.signingSecret);
+console.log('socketMode:', socket_server.socketMode);
+console.log('appToken:', socket_server.appToken);
 function sendDataToClient(data) {
   io.emit('chat', data);
 }
 
 const http_server = http.createServer();
 const io = require('socket.io')(http_server, {
-  perMessageDeflate :false,
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
@@ -25,19 +29,18 @@ const io = require('socket.io')(http_server, {
 
 io.on('connection', async socket => {
   console.log('Client connected');
-  socket.on('sendMessage', async ({ channel, text }) => {
-    // sendMessageToChannel 함수 호출 시 제공된 채널과 텍스트 사용
-    await sendMessageToChannel(channel, text);
-  });
-  // 채널의 메시지 가져오기
-  const channelId = process.env.SOCKET_CHANNEL; // 실제 채널 ID로 대체해야 합니다.
-  await fetchChannelHistory(channelId);
+  const channelID = process.env.SOCKET_CHANNEL;
+  await fetchChannelHistory(channelID)
+  // socket.on('sendMessage', async ({ channel, text }) => {
+  //   // sendMessageToChannel 함수 호출 시 제공된 채널과 텍스트 사용
+  //   await sendMessageToChannel(channel, text);
+  // });
+
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
-const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
 async function sendMessageToChannel(channel, text) {
   try {
     const result = await slackClient.chat.postMessage({
@@ -50,6 +53,7 @@ async function sendMessageToChannel(channel, text) {
     console.error('메시지 전송 중 오류 발생:', error);
   }
 }
+//슬랙에서 서버로
 async function fetchChannelHistory(channelId) {
   let data = [];
   try {
@@ -57,6 +61,7 @@ async function fetchChannelHistory(channelId) {
       channel: channelId,
       limit: 100,
     });
+    console.log("FetchChannelHISTOTY오냐")
     for (let i = response.messages.length - 1; i >= 0; i--) {
       
       let lines = response.messages[i].text.split('\n');
@@ -66,7 +71,9 @@ async function fetchChannelHistory(channelId) {
       };
       data.push(element);
     }
+    console.log("FetchannelHISTORY마지막시련")
     sendDataToClient(data);
+    console.log("FetchChannelHISTOTY다옴")
   } catch (error) {
     console.error('Error fetching channel history:', error);
   }
@@ -75,7 +82,7 @@ async function fetchChannelHistory(channelId) {
 // Slack 앱에서 `message.channels` 이벤트 구독
 socket_server.event('message', async ({ event, client }) => {
   const channelId = event.channel;
-  console.log(channelId)
+  console.log(event.message, "신호가 감")
   await fetchChannelHistory(channelId);
 });
 
